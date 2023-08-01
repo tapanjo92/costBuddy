45d04da5aaaae57c244e57c354e5a723062d1a15
tapanjo92 <49943773+tapanjo92@users.noreply.github.com>
2023-08-01 11:50:00 +0530

    Update prometheus.tf

--- terraform/modules/prometheus/prometheus.tf
+++ terraform/modules/prometheus/prometheus.tf
@@ -1,18 +1,11 @@
-
 locals {
   common_tags = {
     Name = "costbuddy-monitoring-server"
   }
 }
 
-
-data "external" "getip" {
-  program = ["bash", "./modules/prometheus/get_ip.sh"]
-}
-
 locals {
-  cidr_admin_whitelist_all = concat(var.cidr_admin_whitelist, lookup(data.external.getip.result, "result", null) == null ? [] : tolist([lookup(data.external.getip.result, "result")]))
-
+  cidr_admin_whitelist_all = var.cidr_admin_whitelist
 }
 
 # Uploads a new keypair
@@ -28,14 +21,13 @@ data "aws_subnet" "ingress_subnet" {
   id    = var.ingress_subnet_id
 }
 
-
 # Get the subnet details including VPC id.
 data "aws_subnet" "private_subnet" {
   count = var.parent ? 1 : 0
   id    = var.private_subnet_id
 }
 
-#Creates S3 bucket for costbuddy
+# Creates S3 bucket for costbuddy
 resource "aws_s3_bucket" "out_bucket" {
   count         = var.parent ? 1 : 0
   bucket        = var.costbuddy_output_bucket
@@ -43,7 +35,6 @@ resource "aws_s3_bucket" "out_bucket" {
   force_destroy = true
   tags          = merge(local.common_tags, var.tags)
 }
-
 
 # Create user data file using templates.
 data "template_file" "user_data" {
@@ -62,7 +53,6 @@ data "archive_file" "costbuddy_lambda_zip" {
   output_path = "/tmp/artifacts.zip"
   type        = "zip"
   source_dir  = "../docker_compose"
-
 }
 
 resource "aws_s3_bucket_object" "costbuddy_artifacts_object" {
@@ -99,9 +89,7 @@ resource "aws_instance" "prometheus" {
   tags = merge(local.common_tags, var.tags)
 }
 
-
 data "aws_availability_zones" "available" {}
-
 
 # Security group rules for ssh access to prometheus and grafana servers
 resource "aws_security_group" "costbuddy_ssh_access" {
@@ -189,33 +177,13 @@ resource "aws_security_group" "costbuddy_external_http_traffic" {
   tags = merge(local.common_tags, var.tags)
 }
 
-# Creates a Elastic IP address
-resource "aws_eip" "eip_prometheus" {
-
-  count = var.parent ? 1 : 0
-  vpc   = true
-  tags  = merge(local.common_tags, var.tags)
-}
-
-# Associates the Elastic IP address to the Prometheus instance
-resource "aws_eip_association" "eip_assoc" {
-
-  count         = var.parent ? 1 : 0
-  instance_id   = aws_instance.prometheus[0].id
-  allocation_id = aws_eip.eip_prometheus[0].id
-
-}
-
-
 data "aws_route53_zone" "costbuddy" {
   count = var.hosted_zone_name_exists && var.zone_name != "" ? 1 : 0
   name  = var.zone_name
 }
 
-
 # Creates a Route53 zone record to access grafana and prometheus
 resource "aws_route53_zone" "metrics" {
-
   count = var.parent && var.hosted_zone_name_exists == false && var.zone_name != "" ? 1 : 0
   name  = var.zone_name
 
@@ -224,15 +192,13 @@ resource "aws_route53_zone" "metrics" {
 
 # Creates a Route53 zone record to access grafana and prometheus
 resource "aws_route53_record" "prometheus_www" {
-
   count   = var.parent && var.www_domain_name != "" ? 1 : 0
   zone_id = var.hosted_zone_name_exists ? data.aws_route53_zone.costbuddy[0].zone_id : aws_route53_zone.metrics[0].zone_id
   #  zone_id = aws_route53_zone.metrics[0].zone_id
   name    = var.www_domain_name
   type    = "A"
   ttl     = "3600"
-  records = [aws_eip.eip_prometheus[0].public_ip]
-
+  records = [aws_instance.prometheus[0].private_ip]
 }
 
 # Creates an instance profile for Prometheus server
@@ -263,7 +229,7 @@ resource "aws_iam_role" "iam_for_monitoring" {
   ]
 }
 EOF
-  tags               = merge(local.common_tags, var.tags)
+  tags = merge(local.common_tags, var.tags)
 }
 
 # Creates an IAM policy for instance profile
